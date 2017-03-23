243a7e329aa457c17b4564207b91b74bdc1a2369
lihengming <lihengming@duia.com>
2017-03-23 11:07:24 +0800

    Initial commit

---
+++ src/main/java/com/example/autocinfigure/ExampleService.java
@@ -0,0 +1,15 @@
+package com.example.autocinfigure;
+
+public class ExampleService {
+
+    private String prefix;
+    private String suffix;
+
+    public ExampleService(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+    public String wrap(String word) {
+        return prefix + word + suffix;
+    }
+}
